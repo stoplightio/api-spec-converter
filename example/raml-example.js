57acf24c5859dd68d7398c05385bac4ebfbf4847
Md Ali Ahsan Rana <rana_cse_ruet@yahoo.com>
2015-09-09 01:25:03 -0400

    - example support for raml - query string support for raml.

--- example/raml-example.js
+++ example/raml-example.js
@@ -1,21 +1,24 @@
 var slConverter = require('../index')
-
+var fs = require('fs')
 var ramlConverter = new slConverter.Converter('raml')
 
-ramlConverter.load('./example.raml', function(){
-  //console.log('Groups:')
-  //console.log(ramlConverter.getSLEndpointGroups())
-
-  console.log('Endpoints:')
-  try {
-    var endpoints = ramlConverter.getSLEndpoints()
-    for (var index in endpoints) {
-      if (endpoints[index].request.bodies) {
-        console.log(endpoints[index].request.bodies[0])
+try{
+  var data = fs.readFileSync('./example.raml', 'utf8')
+  ramlConverter.load(data, function(){
+    console.log('Endpoints:')
+    try {
+      var endpoints = ramlConverter.getSLEndpoints()
+      for (var index in endpoints) {
+        if (endpoints[index].request.bodies) {
+          console.log(endpoints[index].request.queryString)
+        }
       }
     }
-  }
-  catch(err) {
-    console.log(err)
-  }
-})
+    catch(err) {
+      console.log(err)
+    }
+  })
+}
+catch(err) {
+  console.log(err)
+}
