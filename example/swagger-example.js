df9d719a1f6432a1a8e2ad5d5f7616026668d67a
Md Ali Ahsan Rana <rana_cse_ruet@yahoo.com>
2015-09-22 01:17:58 -0400

    - add response body/schema for swagger export.

--- example/swagger-example.js
+++ example/swagger-example.js
@@ -3,11 +3,12 @@ var fs = require('fs')
 
 var ramlConverter = new slConverter.Converter(slConverter.Formats.SWAGGER, slConverter.Formats.SWAGGER)
 
-try {
-  ramlConverter.loadFile(__dirname + '/swagger.yaml')(function(){
-    console.log(JSON.stringify(ramlConverter.getConvertedData(), null, 2))
-  })
-}
-catch(err) {
-  console.log("Error", err)
-}
+ramlConverter.loadFile(__dirname + '/swagger.yaml')(function(){
+  try{
+    //ramlConverter.getConvertedData()
+    fs.writeFileSync(__dirname + '/test.json', JSON.stringify(ramlConverter.getConvertedData(), null, 2), 'utf8')
+  }
+  catch(err) {
+    console.log(err)
+  }
+})
