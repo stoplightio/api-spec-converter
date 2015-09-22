b39ebcdf238be99c7734b3d74b5cc3e6bab897c2
Md Ali Ahsan Rana <rana_cse_ruet@yahoo.com>
2015-09-22 00:12:40 -0400

    - Swagger export, basic valid schema(in progress).

--- example/swagger-example.js
+++ example/swagger-example.js
@@ -1,11 +1,11 @@
 var slConverter = require('../index')
 var fs = require('fs')
 
-var ramlConverter = new slConverter.Converter(slConverter.Formats.SWAGGER, slConverter.Formats.STOPLIGHT)
+var ramlConverter = new slConverter.Converter(slConverter.Formats.SWAGGER, slConverter.Formats.SWAGGER)
 
 try {
-  ramlConverter.loadFile('./swagger.yaml')(function(){
-    console.log(ramlConverter.getConvertedData())
+  ramlConverter.loadFile(__dirname + '/swagger.yaml')(function(){
+    console.log(JSON.stringify(ramlConverter.getConvertedData(), null, 2))
   })
 }
 catch(err) {
