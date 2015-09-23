85792b75cf46ff000d0c21540ecfa269035a829a
Md Ali Ahsan Rana <rana_cse_ruet@yahoo.com>
2015-09-23 01:18:00 -0400

    - support for export to yaml format.

--- example/swagger-example.js
+++ example/swagger-example.js
@@ -6,7 +6,7 @@ var ramlConverter = new slConverter.Converter(slConverter.Formats.SWAGGER, slCon
 ramlConverter.loadFile(__dirname + '/swagger.yaml')(function(){
   try{
     //ramlConverter.getConvertedData()
-    fs.writeFileSync(__dirname + '/test.json', JSON.stringify(ramlConverter.getConvertedData(), null, 2), 'utf8')
+    fs.writeFileSync(__dirname + '/test.yaml', ramlConverter.getConvertedData('yaml'), 'utf8')
   }
   catch(err) {
     console.log(err)
