a564137f325e619fbf1638f9ed1dca9b5fcd496c
Md Ali Ahsan Rana <rana_cse_ruet@yahoo.com>
2015-09-23 01:48:50 -0400

    - some null data handling.

--- example/swagger-example.js
+++ example/swagger-example.js
@@ -1,14 +1,14 @@
 var slConverter = require('../index')
 var fs = require('fs')
 
-var ramlConverter = new slConverter.Converter(slConverter.Formats.SWAGGER, slConverter.Formats.SWAGGER)
+var ramlConverter = new slConverter.Converter(slConverter.Formats.RAML, slConverter.Formats.SWAGGER)
 
-ramlConverter.loadFile(__dirname + '/swagger.yaml')(function(){
+ramlConverter.loadFile(__dirname + '/example.raml')(function(){
   try{
     //ramlConverter.getConvertedData()
     fs.writeFileSync(__dirname + '/test.yaml', ramlConverter.getConvertedData('yaml'), 'utf8')
   }
   catch(err) {
-    console.log(err)
+    console.log(err.stack)
   }
 })
