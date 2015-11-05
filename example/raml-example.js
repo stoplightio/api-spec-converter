8226902fff8f81e764df7c0bf89c45295007e47d
Rana Md Ali Ahsan <rana@stoplight.io>
2015-11-05 18:18:15 -0500

    - basic draft.

--- example/raml-example.js
+++ example/raml-example.js
@@ -3,10 +3,11 @@ var fs = require('fs')
 var ramlConverter = new slConverter.Converter(slConverter.Formats.RAML, slConverter.Formats.RAML)
 
 try {
-  ramlConverter.loadFile('./source/example.raml', function(){
+  //./source/raml.yaml
+  ramlConverter.loadFile('./source/raml.yaml', function(){
     try{
       //console.log(ramlConverter.getSLSchemas())
-      fs.writeFileSync(__dirname + '/exported-raml.yaml', ramlConverter.getConvertedData('yaml'), 'utf8')
+      fs.writeFileSync(__dirname + '/exported-raml.json', JSON.stringify(ramlConverter.getConvertedData('json'), null, 2), 'utf8')
     }
     catch(err) {
       console.log(err.stack)
@@ -16,5 +17,13 @@ try {
 catch(err) {
   console.log("Error", err)
 }
+
+/*var raml = require('raml-parser');
+
+raml.loadFile('./source/raml.yaml').then( function(data) {
+  fs.writeFileSync(__dirname + '/raml.json', JSON.stringify(data, null, 2), 'utf8')
+}, function(error) {
+  console.log('Error parsing: ' + error);
+});*/
 
 
