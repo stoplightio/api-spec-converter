5027405b58cda650d0f05858210ca7bbe17bc787
Rana Md Ali Ahsan <rana@stoplight.io>
2015-11-06 18:44:19 -0500

    - protocols/default request/response type support in swagger/raml.

--- example/swagger-example.js
+++ example/swagger-example.js
@@ -3,10 +3,10 @@ var fs = require('fs')
 
 var ramlConverter = new slConverter.Converter(slConverter.Formats.SWAGGER, slConverter.Formats.SWAGGER)
 
-ramlConverter.loadFile(__dirname + '/swagger.yaml')(function(){
+ramlConverter.loadFile(__dirname + '/source/swagger.yaml', function(){
   try{
     //console.log(ramlConverter.getSLSchemas())
-    fs.writeFileSync(__dirname + '/test.yaml', ramlConverter.getConvertedData('yaml'), 'utf8')
+    fs.writeFileSync(__dirname + '/exported-swagger.yaml', ramlConverter.getConvertedData('yaml'), 'utf8')
   }
   catch(err) {
     console.log(err.stack)
