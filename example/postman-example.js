a948d94b15814f62e43d9894455d58e7abac2353
Rana Md Ali Ahsan <rana@stoplight.io>
2015-11-08 01:20:39 -0500

    - fix postman example - minor refactoring.

--- example/postman-example.js
+++ example/postman-example.js
@@ -2,10 +2,10 @@ var slConverter = require('../index')
 
 var pmConverter = new slConverter.Converter(slConverter.Formats.POSTMAN, slConverter.Formats.STOPLIGHT)
 
-pmConverter.loadFile('./postman.json')
+pmConverter.loadFile('./source/postman.json', function(){
+  console.log('Groups:')
+  console.log(pmConverter.getImportedProject().Groups)
 
-console.log('Groups:')
-console.log(pmConverter.getImportedProject().Groups)
-
-console.log('Endpoints:')
-console.log(pmConverter.getImportedProject().Endpoints)
+  console.log('Endpoints:')
+  console.log(pmConverter.getImportedProject().Endpoints)
+})
