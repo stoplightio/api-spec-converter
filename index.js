93702a4f2543c2fa2d687fbca0e8b6c737286f77
Rana Md Ali Ahsan <rana@stoplight.io>
2015-11-08 01:26:30 -0500

    - small fix. - little refactoring.

--- index.js
+++ index.js
@@ -1,6 +1,25 @@
 var converter = require('./lib/converter')
 
+var supportedFormats = {
+  'POSTMAN': {
+    name: 'Postman',
+    className: 'Postman'
+  },
+  'RAML':  {
+    name: 'RAML',
+    className: 'RAML'
+  },
+  'SWAGGER': {
+    name: 'Swagger',
+    className: 'Swagger'
+  },
+  'STOPLIGHT': {
+    name: 'StopLight',
+    className: 'StopLight'
+  }
+}
+
 module.exports = {
   Converter: converter.Converter,
-  Formats: converter.Formats
+  Formats: supportedFormats,
 }
