21086cd3823ba9d914c9c4c358b13d0232549d03
Rana Md Ali Ahsan <rana@stoplight.io>
2015-11-09 01:16:55 -0500

    - Make stoplight export/import consistent. - minor refactorings.

---
+++ example/stoplight-example.js
@@ -0,0 +1,18 @@
+var slConverter = require('../index')
+var fs = require('fs')
+var slConverter = new slConverter.Converter(slConverter.Formats.STOPLIGHT, slConverter.Formats.STOPLIGHT)
+
+try {
+  slConverter.loadFile(__dirname + '/stoplight.json', function(){
+    try{
+      console.log(slConverter.convert('json'))
+      fs.writeFileSync(__dirname + '/exported-stoplight.json', slConverter.convert('json'), 'utf8')
+    }
+    catch(err) {
+      console.log(err.stack)
+    }
+  })
+}
+catch(err) {
+  console.log("Error", err)
+}
