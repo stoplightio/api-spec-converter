9241e47fa61313547ee13384520410708f0ca85a
Rana Md Ali Ahsan <rana@stoplight.io>
2015-11-12 20:01:17 -0500

    - run gulp task on travis.

--- gulpfile.js
+++ gulpfile.js
@@ -1,14 +1,20 @@
 var gulp = require('gulp'),
-    eslint = require('gulp-eslint');
+    eslint = require('gulp-eslint'),
+    coveralls = require('gulp-coveralls');
 
 gulp.task('lint', function(done) {
   // patterns with the same form as gulp.src(patterns)
   var patterns = ['lib/*.js'];
 
-  return gulp.src(['**/*.js','!node_modules/**'])
+  return gulp.src(['**/*.js','!node_modules/**',,'!coverage/**'])
         .pipe(eslint())
         .pipe(eslint.format())
         .pipe(eslint.failAfterError());
 });
 
-gulp.task('default', ['lint']);
+gulp.task('coverall', function(){
+  gulp.src('coverage/lcov.info')
+  .pipe(coveralls());
+});
+
+gulp.task('default', ['lint', 'coverall']);
