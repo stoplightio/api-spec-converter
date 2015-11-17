2f7a13469d2180b0ef3adb96e6629fd0efd84cbb
Rana Md Ali Ahsan <rana@stoplight.io>
2015-11-17 01:36:36 -0500

    - test to verify exported swagger correctness.

--- test/lib/exporters/swagger.js
+++ test/lib/exporters/swagger.js
@@ -1,7 +1,13 @@
 var expect   = require('chai').expect,
-    RAML = require('../../../lib/exporters/swagger');
+    Swagger = require('../../../lib/exporters/swagger'),
+    parser = require('swagger-parser'),
+    fs = require('fs');
 
 describe('Swagger Exporter', function(){
+  var swaggerExporter;
+  beforeEach(function(){
+    swaggerExporter = new Swagger();
+  });
 
   describe('constructor', function(){
     it('should return valid Swagger instance');
@@ -9,7 +15,19 @@ describe('Swagger Exporter', function(){
   });
 
   describe('_export', function(){
-    it('should perform export for loaded data');
+    it('should perform export for loaded data', function(done){
+      swaggerExporter.loadSLData(require(__dirname+'/../../data/stoplight.json'));
+      var exportedData = swaggerExporter.export('yaml');
+      fs.writeFileSync('temp.yaml', exportedData, 'utf8');
+      parser.parse('temp.yaml')
+      .then(function(api, metadata) {
+        done();
+      })
+      .catch(function(err) {
+        expect(err).to.equal(undefined);
+        done();
+      });
+    });
   });
 
   //TODO test internal methods individually
