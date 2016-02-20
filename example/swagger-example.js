878fc5cbac8547d7485145e07aec40dbc5cc324a
Rana Md Ali Ahsan <rana@stoplight.io>
2016-02-20 01:00:27 -0500

    - example fix

--- example/swagger-example.js
+++ example/swagger-example.js
@@ -2,21 +2,22 @@ var slConverter = require('../index'),
     fs = require('fs');
 
 var swaggerConverter = new slConverter.Converter(slConverter.Formats.SWAGGER, slConverter.Formats.SWAGGER);
-var data = fs.readFileSync(__dirname + '/source/swagger.yaml', 'utf8');
+var filePath = __dirname + '/source/swagger.yaml';
+var data = fs.readFileSync(filePath, 'utf8');
 
-swaggerConverter.loadData(data, function(err){
-  if(err) {
-    return console.log(err);
-  }
-  try{
+swaggerConverter.loadData(data)
+.then(function(){
     swaggerConverter.convert('yaml', function(err, exportedData){
       if (err) {
         return console.log(err);
       }
       fs.writeFileSync(__dirname + '/target/swagger.yaml', exportedData, 'utf8');
     });
-  }
-  catch(err) {
-    console.log(err.stack);
+})
+.catch(function(err){
+  if(err) {
+    return console.log(err);
   }
 });
+
+
