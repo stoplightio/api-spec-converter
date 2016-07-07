f572c4424666ca8527988a6ae04254eda02b3ffa
Vazha Omanashvili <rainum.ua@gmail.com>
2016-07-07 14:13:49 +0300

    fix jslint warnings

--- gulpfile.js
+++ gulpfile.js
@@ -2,19 +2,16 @@ var gulp = require('gulp'),
     eslint = require('gulp-eslint'),
     coveralls = require('gulp-coveralls');
 
-gulp.task('lint', function(done) {
-  // patterns with the same form as gulp.src(patterns)
-  var patterns = ['lib/*.js'];
-
-  return gulp.src(['**/*.js','!node_modules/**',,'!coverage/**'])
-        .pipe(eslint())
-        .pipe(eslint.format())
-        .pipe(eslint.failAfterError());
+gulp.task('lint', function() {
+  return gulp.src(['**/*.js', '!node_modules/**', '!coverage/**'])
+    .pipe(eslint())
+    .pipe(eslint.format())
+    .pipe(eslint.failAfterError());
 });
 
-gulp.task('coverall', function(){
+gulp.task('coverall', function() {
   gulp.src('coverage/lcov.info')
-  .pipe(coveralls());
+    .pipe(coveralls());
 });
 
 gulp.task('default', ['lint', 'coverall']);
