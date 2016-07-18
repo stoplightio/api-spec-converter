ffc94d69df94c29ae8d3b94cf0319a5f65a76a59
Vazha Omanashvili <rainum.ua@gmail.com>
2016-07-18 14:45:05 +0300

    add basic tests coverage for tests

--- test/lib/exporters/stoplightx.js
+++ test/lib/exporters/stoplightx.js
@@ -1,30 +1,60 @@
-var expect   = require('chai').expect,
+var expect = require('chai').expect,
     StoplightX = require('../../../lib/exporters/stoplightx'),
+    StoplightXImporter = require('../../../lib/importers/stoplightx'),
     Project = require('../../../lib/entities/project');
 
-describe('Stoplight Exporter', function(){
+describe('Stoplight Exporter', function() {
   var exporter;
-  before(function(){
+  var importer;
+  var filePath = __dirname + '/../../data/stoplightx.json';
+
+  before(function() {
     exporter = new StoplightX();
+    importer = new StoplightXImporter();
   });
-  describe('constructor', function(){
-    it('create new instance of StoplightX exporter successfully', function(){
+
+  describe('constructor', function() {
+    it('create new instance of StoplightX exporter successfully', function() {
       expect(exporter).to.be.an.instanceof(StoplightX);
     });
   });
-  describe('_export', function(){
-    it('should export project to data', function(){
+
+  describe('_export', function() {
+    it('should export project to data', function() {
       expect(exporter.data).to.equal(null);
       //pre-requisite
       exporter.loadProject(new Project('testProject'));
       exporter._export();
       expect(exporter.data).to.not.equal(null);
     });
-    it('exported data should be of swagger format', function(){
+
+    it('exported data should be of swagger format', function() {
       exporter.loadProject(new Project('testProject'));
       exporter._export();
       expect(exporter.data).to.include.keys('swagger');
       expect(exporter.data).to.be.an('object');
+    });
+
+    it('should export tests', function(done) {
+      importer.loadFile(filePath, function(err) {
+        if (err) {
+          return done(err);
+        }
+
+        importer.import();
+
+        exporter.loadProject(importer.project);
+        exporter._export();
+
+        var tests = exporter.data['x-tests'];
+
+        expect(tests).to.be.an('object');
+        expect(Object.keys(tests)).to.have.lengthOf(5);
+        expect(tests['WuCyPM8JXTvGAGKHr'].steps[1])
+          .to.have.property('$ref', '#/x-tests/SuDCFmBBcvmyA7dCh');
+
+        done();
+      });
     });
   });
 });
